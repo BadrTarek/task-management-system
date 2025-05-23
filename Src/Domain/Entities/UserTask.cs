5436fc70e6a606593a2ac75c1893d0cddddb09fa
BadrTarek <badr.tarek20@gmail.com>
2025-05-24 00:23:59 +0300

    add project entities

---
+++ Src/Domain/Entities/UserTask.cs
@@ -0,0 +1,17 @@
+using Domain.Enums;
+
+namespace Domain.Entities
+{
+    public class UserTask
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime DueDate { get; set; }
+        public UserTaskStatuses Status { get; set; }
+        public int UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
