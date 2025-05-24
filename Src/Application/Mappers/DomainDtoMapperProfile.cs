911847fa9c9f9978595fb0026d38c03a9fa09629
BadrTarek <badr.tarek20@gmail.com>
2025-05-24 16:05:30 +0300

    add user tasks mapper

--- Src/Application/Mappers/DomainDtoMapperProfile.cs
+++ Src/Application/Mappers/DomainDtoMapperProfile.cs
@@ -1,6 +1,8 @@
 using Application.Dtos.UserDtos;
+using Application.UserTaskDtos.Dtos;
 using AutoMapper;
 using UserEntity = Domain.Entities.User;
+using UserTaskEntity = Domain.Entities.UserTask;
 
 namespace Data.Mappers
 {
@@ -10,6 +12,9 @@ namespace Data.Mappers
         {
             CreateMap<UserEntity, UserDto>().ReverseMap();
             CreateMap<UserEntity, SignupDto>().ReverseMap();
+            CreateMap<UserTaskEntity, UserTaskDto>().ReverseMap();
+            CreateMap<IEnumerable<UserTaskEntity>, IEnumerable<UserTaskDto>>().ReverseMap();
+            CreateMap<CreateUserTaskDto, UserTaskEntity>().ReverseMap();
         }
     }
 }
